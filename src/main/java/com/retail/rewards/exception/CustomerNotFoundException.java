92f038b64bb774439679b8a1cb338115519462a5
gouravgajara <gouravgajara@gmail.com>
2026-05-26 14:12:30 +0530

    Update CustomerNotFoundException.java

--- src/main/java/com/retail/rewards/exception/CustomerNotFoundException.java
+++ src/main/java/com/retail/rewards/exception/CustomerNotFoundException.java
@@ -1,3 +1,7 @@
 package com.retail.rewards.exception;
 
-public class CustomerNotFoundException extends RuntimeException { public CustomerNotFoundException(String message){ super(message);} }
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
